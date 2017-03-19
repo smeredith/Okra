a653b1cf68cd7627db2d514a4b4a364a77d41046
Jay Bazuzi <JAY@BAZUZI.COM>
2017-03-19 16:12:44 -0700

    build.cmd - stop on error

--- build.cmd
+++ build.cmd
@@ -1,6 +1,12 @@
 @SETLOCAL
 @IF NOT EXIST .build mkdir .build
 cd .build
+
 cmake -Werror=dev -Werror=deprecated "%~dp0"
+IF ERRORLEVEL 1 goto :EOF
+
 cmake --build . -- /verbosity:minimal
+IF ERRORLEVEL 1 goto :EOF
+
 ctest
+IF ERRORLEVEL 1 goto :EOF
