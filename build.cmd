5e23b017655ea65a3af45fa0acde730957bb6c8b
Jay Bazuzi <JAY@BAZUZI.COM>
2017-03-19 16:14:59 -0700

    build.cmd - echo step headings

--- build.cmd
+++ build.cmd
@@ -1,12 +1,16 @@
 @SETLOCAL
+@ECHO OFF
 @IF NOT EXIST .build mkdir .build
 cd .build
 
+echo ******************** cmake ********************
 cmake -Werror=dev -Werror=deprecated "%~dp0"
 IF ERRORLEVEL 1 goto :EOF
 
+echo ******************** build ********************
 cmake --build . -- /verbosity:minimal
 IF ERRORLEVEL 1 goto :EOF
 
+echo ******************** ctest ********************
 ctest
 IF ERRORLEVEL 1 goto :EOF
